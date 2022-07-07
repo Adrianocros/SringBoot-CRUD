6d11d7decbb11a10f0754f8bf8ea5184f8b6f8b3
Adriano Bianchi Crosoletto <adrianobianchi@outlook.com>
2022-07-06 21:03:36 -0300

    Criando exceções para Update

--- src/main/java/com/projetocrud/crud/services/UserService.java
+++ src/main/java/com/projetocrud/crud/services/UserService.java
@@ -11,6 +11,7 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 
@@ -47,9 +48,13 @@ public class UserService {
 
     //Atualizar o usuario
     public User update(Long id, User obj){
-        User entity = repository.getById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        }catch (EntityNotFoundException e){
+            throw  new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
@@ -57,5 +62,4 @@ public class UserService {
         entity.setEmail(obj.getEmail());
         entity.setPhone(obj.getPhone());
     }
-
 }
